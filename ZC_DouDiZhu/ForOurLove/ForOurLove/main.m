8917c6ee29d855cbc0d9d5e45283e63d6bf4ff04
iijy <zc_1712@126.com>
2013-12-02 18:31:24 +0800

    Initial Commit

---
+++ ZC_DouDiZhu/ForOurLove/ForOurLove/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ForOurLove
+//
+//  Created by Iijy ZC on 13-12-2.
+//  Copyright (c) 2013年 Iijy ZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZCAppDelegate class]));
+    }
+}
---
+++ ZC_DouDiZhu/ForOurLove/ForOurLoveTests/ForOurLoveTests.m
@@ -0,0 +1,34 @@
+//
+//  ForOurLoveTests.m
+//  ForOurLoveTests
+//
+//  Created by Iijy ZC on 13-12-2.
+//  Copyright (c) 2013年 Iijy ZC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ForOurLoveTests : XCTestCase
+
+@end
+
+@implementation ForOurLoveTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
